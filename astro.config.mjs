a986d34dc89ceceaf187172c60100b25d85e4b43
LouisMaucourt <louis.maucourt7@gmail.com>
2025-06-16 14:35:30 +0200

    firstcommit goes brrrrrrr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import sanity from '@sanity/astro';
-import react from '@astrojs/react';
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanity(), react()]
+	integrations: [
+		sanity({
+			projectId: "a7jn423a",
+			dataset: "studio-refuge-canin-solidaire",
+			// Set useCdn to false if you're building statically.
+			useCdn: false,
+		}),
+	],
 });
