d9654fe1fe5cfc090ca7cd14f5d4bf00f054ea48
Maxime Lust <maxime@favoreatdesign.com>
2025-06-20 10:17:46 +0200

    remove vercel output directory

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, s as start } from './chunks/entrypoint_BodRE9Eh.mjs';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_DQuNqJ_X.mjs';
-export { renderers } from '../renderers.mjs';
